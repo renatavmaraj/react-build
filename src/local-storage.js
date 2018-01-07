badbf694a8e5656d6a65b2e7cc1738edc3303d2b
Chance Eakin <chance.eakin@gmail.com>
2018-01-06 22:42:21 -0600

    added localstorage stuff and also flow-typing for when i finally decide to remove decorators

---
+++ src/local-storage.js
@@ -0,0 +1,24 @@
+/*
+ * Utility functions that save/load redux state to/from localStorage.
+ */
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.error(e)
+    return undefined
+  }
+}
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.error(e)
+  }
+}
